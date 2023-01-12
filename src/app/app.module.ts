5ff00f48269995e190ccdedd8f89d7d795191933
CAPED_CRUSADER01 <sriramsridhar26@gmail.com>
2023-01-12 12:45:19 +0530

    Redesigned every page to look good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,21 @@ import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
 import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
 import { DropzoneDirective } from './directives/dropzone.directive';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +44,20 @@ import { DropzoneDirective } from './directives/dropzone.directive';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
